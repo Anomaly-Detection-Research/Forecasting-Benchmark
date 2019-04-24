285144801ebaf81437a8e0df252fdc84438ff0c1
Pasindu Perera <pasindubawantha@gmail.com>
2019-04-24 11:22:33 +0530

    added setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+apt-get --yes --force-yes install python-pip
+pip install -U matplotlib==2.0
+pip install statsmodels
+pip install pandas
+pip install scipy
+pip install numpy
+pip install tensorflow
+pip install keras
